8f9e85358f87d39b03a7aacb4bab7d6f1ea84114
GapVR <93127634+GapVR@users.noreply.github.com>
2026-09-24 08:56:02 +0000

    Update TrayIcon.h

--- include/TrayIcon.h
+++ include/TrayIcon.h
@@ -2,8 +2,6 @@
 
 #include <windows.h>
 #include <gdiplus.h>
-#include <vector>
-#include <deque>
 #include "GpuMonitor.h"
 
 class TrayIcon {
@@ -14,19 +12,15 @@ public:
     bool Initialize();
     bool RestoreAfterExplorerRestart();
     void Update(const SystemStats& stats);
-    void SetActiveMetrics(const std::vector<Metric>& metrics) { m_activeMetrics = metrics; }
-    void ShowContextMenu();
 
 private:
     HICON CreateDynamicIcon(const SystemStats& stats);
-    Gdiplus::Color GetColorForUsage(float usage);
-    void DrawGraph(Gdiplus::Graphics& g, float value, int yOffset, int height, Gdiplus::Color color);
 
     HWND m_hWnd;
-    GpuMonitor* m_monitor;
     NOTIFYICONDATA m_nid{};
     bool m_iconAdded = false;
-    
+
     ULONG_PTR m_gdiplusToken;
-    std::vector<Metric> m_activeMetrics = { Metric::GPU, Metric::GPU_MEM };
+    ULONGLONG m_digitTick = 0;
+    int m_digitCycle = 0;
 };
